14a8e035cd3ed726e7044b68f85e6635a2c81b27
David <mrriptidemc@gmail.com>
2021-10-02 23:37:21 -0400

    everything works in a very ugly way
    still need to make the recoil harder to  predict with shaking on both x and y

---
+++ Asteroid.gd
@@ -0,0 +1,45 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+var direction = 0
+var speed = 10
+var distance = 1000
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	var x_range = Vector2(0, get_viewport().get_visible_rect().size[0])
+	var y_range = Vector2(0, get_viewport().get_visible_rect().size[1])
+	
+	self.position = Vector2(x_range[1]/2, y_range[1])
+	
+	# don't allow the asteroids to be spawned within a certain radius of the gun
+	# this is mostly just to prevent having to deal with asteroids being hidden behind the gun
+	while (self.position.distance_to(get_node("../../Gunner").position) < 200):
+		var random_x = randi() % int(x_range[1] - x_range[0]) + 1 + x_range[0]
+		var random_y = randi() % int(y_range[1] - y_range[0]) + 1 + y_range[0]
+		self.position = Vector2(random_x, random_y)
+	# print(self.position)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# show a warning with distance when within 500m of the ship
+	if distance < 500:
+		get_node("Distance").text = str(round(distance)) + "m"
+		get_node("Distance").visible = true
+	# asteroid has hit the ship, deal damage and destroy the asteroid
+	# TODO: add animations for the ship being hit
+	if distance < 0:
+		get_node("../../").damage()
+		self.get_parent().remove_child(self)
+	distance -= speed * delta
+	
+	var percent_size = (distance / 1000)
+	get_node("CollisionShape2D").scale = Vector2(1-percent_size, 1-percent_size)
+
+func damage():
+	print("[", name, "] ive been hit!")
+	get_parent().remove_child(self)
---
+++ Gamespace.gd
@@ -0,0 +1,70 @@
+extends Node2D
+
+onready var asteroidScene = preload("res://Asteroid.tscn")
+var max_health = 100
+var health = max_health
+
+
+func _ready():
+	var asteroid = asteroidScene.instance()
+	
+	get_node("Asteroids").add_child(asteroid)
+	#get_node("Gunner/Hit Registration/Bullet Trace").set_as_toplevel(true)
+
+
+func damage():
+	health -= 10
+	print("Current health: " + str(health))
+
+var y_offset = 0
+var x_offset = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# reducing the recoil
+	var cooldown_rate = 50
+	y_offset = max(0, y_offset - cooldown_rate * delta)
+	x_offset = max(0, x_offset - cooldown_rate * delta)
+	# functionality for shooting the gun
+	#print(y_offset, " , ", x_offset)
+	if Input.is_action_pressed("fire") and get_node("Gunner/Fire Rate").time_left == 0:
+		# TODO: add animation of the gun firing
+		# TODO: add SFX of the gun firing & warming up
+		#print("pew")
+		#print(get_global_mouse_position())
+		#get_node("Gunner/Bullet Trace").global_position = get_global_mouse_position()
+		
+		# doing recoil of the gun
+		
+		var max_x_offset = 150
+		var wiggle_coef = 50
+		# thinking to bound the recoil so that it doesnt end up off the screen
+		y_offset = min(max_x_offset, y_offset + 10 + 25*(1-health/max_health))
+		if y_offset > max_x_offset:
+			x_offset = (1 - health/max_health)*wiggle_coef * sin(y_offset)
+		
+		var hit_reg = get_node("Gunner/Hit Registration")
+		
+		hit_reg.global_position = get_global_mouse_position() - Vector2(
+			x_offset,
+			min(y_offset, max_x_offset + (health/max_health)*wiggle_coef * cos(y_offset))
+		)
+		
+		# hit registration
+		
+		for area in hit_reg.get_overlapping_areas():
+			if "Asteroid" in area.name:
+				area.damage()
+		
+		# since the bullet trace is a child of hit registration it should mark the point that the bullet was fired at
+		get_node("Gunner/Hit Registration/Bullet Trace").emitting = true
+		
+		get_node("Gunner/Fire Rate").start()
+	elif get_node("Gunner/Fire Rate").time_left == 0:
+		get_node("Gunner/Hit Registration/Bullet Trace").emitting = false
+
+
+func _on_Asteroid_Timer_timeout():
+	var asteroid = asteroidScene.instance()
+	
+	get_node("Asteroids").add_child(asteroid)
